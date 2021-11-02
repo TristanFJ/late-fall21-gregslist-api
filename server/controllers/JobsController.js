db8cd2cdff6193cc3d8c536d126358430b173701
Tristan Johns <tfj@protonmail.com>
2021-11-02 15:13:18 -0600

    post new job

--- server/controllers/JobsController.js
+++ server/controllers/JobsController.js
@@ -7,6 +7,9 @@ export class JobsController extends BaseController {
     super('api/jobs')
     this.router
       .get('', this.getAll)
+      .get('/:id', this.getById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.create)
   }
 
   async getAll(req, res, next) {
@@ -14,6 +17,25 @@ export class JobsController extends BaseController {
       const query = req.query
       const jobs = await jobsService.getAll(query)
       return res.send(jobs)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const job = await jobsService.getById(req.params.id)
+      return res.send(job)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const job = await jobsService.create(req.body)
+      return res.send(job)
     } catch (error) {
       next(error)
     }
--- server/services/JobsService.js
+++ server/services/JobsService.js
@@ -6,6 +6,19 @@ class JobsService {
     const jobs = await dbContext.Jobs.find(query).populate('creator', 'name picture')
     return jobs
   }
+
+  async getById(id) {
+    const job = await dbContext.Jobs.findById(id).populate('creator', 'name picture')
+    if (!job) {
+      throw new BadRequest('Invalid Job ID')
+    }
+    return job
+  }
+
+  async create(body) {
+    const job = await dbContext.Jobs.create(body)
+    return job
+  }
 }
 
 export const jobsService = new JobsService()
